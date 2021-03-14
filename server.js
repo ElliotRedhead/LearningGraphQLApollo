11696fa1ba33806f6d32aea410c94698f0c0e9d1
ElliotRedhead <elliot.redhead@gmail.com>
2021-03-14 14:34:51 +0000

    Add basic schema and linting tools..

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
-import { ApolloServer } from "apollo-server";
+import apollo from "apollo-server";
+const { ApolloServer, gql } = apollo;
 
-const server = new ApolloServer();
+const typeDefs = gql`
+	type Query {
+		sessions:[Session]
+		}
+	type Session {
+		id: ID!,
+		title: String!,
+		description:String,
+		startsAt:String,
+		endsAt:String,
+		room:String,
+		day:String,
+		format:String,
+		track:String,
+		level:String,
+}`;
+
+const server = new ApolloServer({ typeDefs });
 
 server
 	.listen({ port: process.env.PORT || 4000 })
