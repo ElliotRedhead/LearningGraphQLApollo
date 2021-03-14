7a94db83fedd532a8282c96672295612b4a0cb89
ElliotRedhead <elliot.redhead@gmail.com>
2021-03-14 12:58:35 +0000

    Create server file to run apollo server.

---
+++ server.js
@@ -0,0 +1,9 @@
+import { ApolloServer } from "apollo-server";
+
+const server = new ApolloServer();
+
+server
+	.listen({ port: process.env.PORT || 4000 })
+	.then(({ url }) => {
+		console.log(`GraphQL running at ${url}`);
+	});
